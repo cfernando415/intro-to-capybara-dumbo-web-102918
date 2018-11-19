8b2871dfbd166e328c51f066a416cb027479a0fb
cfernando415 <ca.fernando415@gmail.com>
2018-11-19 18:35:05 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
+require 'pry'
 class Application < Sinatra::Base
   # Write your code here!
+  configure do
+    set :views, "views"
+  end
+  get '/' do
+    erb :index
+  end
+  post '/greet' do
+    erb :greet
+  end
 
 end
